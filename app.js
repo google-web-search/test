32a9210751cc6d868c627026675dc56d0f206c7a
Shofikul Islam <128067400+google-web-search@users.noreply.github.com>
2023-05-12 15:42:38 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+  
+    var firebaseConfig = {
+    apiKey: "AIzaSyDeOU_XucStcft4sAVUAonm66ZAcjLrVgs",
+    authDomain: "AUTH DOMAIN",
+    databaseURL: "https://oneandonly-d6d6a-default-rtdb.firebaseio.com",
+    projectId: "oneandonly-d6d6a",
+    storageBucket: "oneandonly-d6d6a.appspot.com",
+    messagingSenderId: "508820282657",
+    appId: "APP ID1:508820282657:android:67dd029d3cbc6f82a8b260"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
